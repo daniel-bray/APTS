f0a83c8c0408cd98cca1d9ef88082d1f48c4d0d7
kekkerslawl <braydj@gmail.com>
2020-11-30 12:55:52 -0600

    continuing classes

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+class Department {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  describe() {
+    console.log("Department: " + this.name);
+  }
+}
+
+const accounting = new Department("Accounting");
+
+console.log(accounting);
+
+accounting.describe();
+
+const accountingCopy = { describe: accounting.describe };
+accountingCopy.describe(); // returns undefined. compiles fine
