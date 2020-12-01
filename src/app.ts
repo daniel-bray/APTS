6ec462d48d31b242939f3db748bafa92fdc26b66
kekkerslawl <braydj@gmail.com>
2020-12-01 11:51:31 -0600

    working on classes/interfaces section

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,32 @@
 class Department {
   name: string;
+  employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
   }
 
-  describe() {
+  describe(this: Department) {
     console.log("Department: " + this.name);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
 const accounting = new Department("Accounting");
 
-console.log(accounting);
+accounting.addEmployee("Daniel");
+accounting.addEmployee("Easton");
 
 accounting.describe();
+accounting.printEmployeeInformation();
 
-const accountingCopy = { describe: accounting.describe };
-accountingCopy.describe(); // returns undefined. compiles fine
+// const accountingCopy = { describe: accounting.describe };
+// accountingCopy.describe(); // returns undefined. compiles fine
